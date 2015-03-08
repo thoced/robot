a66ad29bb79fdfe69f28377e5a3f5b1d58f1bc7f
THOCED <thonon_cedric@hotmail.com>
2015-03-08 10:19:43 +0100

    initial commit

---
+++ CoreFx/BaseFx.java
@@ -0,0 +1,56 @@
+package CoreFx;
+
+import org.jsfml.graphics.Drawable;
+import org.jsfml.graphics.RenderStates;
+import org.jsfml.graphics.RenderTarget;
+import org.jsfml.system.Time;
+import org.jsfml.window.event.Event;
+
+import bilou.ICoreBase;
+
+public class BaseFx implements Drawable,ICoreBase 
+{
+
+	@Override
+	public void update(Time deltaTime) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void loadContent() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reloadContent() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteContent() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void catchEvent(Event e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw(RenderTarget arg0, RenderStates arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
